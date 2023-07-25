299852f4217a2910516c9dcd1df9a8c25c47bbb5
Muhammad Al-ameen <muhammadtt97@gmail.com>
2023-07-25 01:15:27 +0100

    updated app.py
    Added user authentication and database

--- app.py
+++ app.py
@@ -1,25 +1,99 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a strong secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 # Simulated database for blog posts
 posts = []
+
+# Create the User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+# Create the Comment model
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('comments', lazy=True))
+
+# Create the Post model
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('posts', lazy=True))
+    tags = db.relationship('Tag', secondary='post_tag', backref=db.backref('posts', lazy=True))
+
+# Create the Tag model
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+# Create the association table for Post and Tag
+post_tag = db.Table('post_tag',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
+
+# Load a user from the database for login
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
     return render_template('index.html', posts=posts)
 
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        author = request.form['author']
-        posts.append({'title': title, 'content': content, 'author': author})
+        tags = [Tag.query.get(int(tag)) for tag in request.form.getlist('tags')]
+        post = Post(title=title, content=content, author=current_user, tags=tags)
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('create.html')
+    return render_template('create.html', tags=Tag.query.all())
+
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get(post_id)
+    if request.method == 'POST':
+        content = request.form['content']
+        comment = Comment(content=content, post=post, author=current_user)
+        db.session.add(comment)
+        db.session.commit()
+    return render_template('post.html', post=post)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
-
